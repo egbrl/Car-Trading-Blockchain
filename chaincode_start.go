4f3472506731c6792e424667e4c519dcb6576a79
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-03-27 14:15:59 +0200

    one file, do not import entities

--- chaincode_start.go
+++ chaincode_start.go
@@ -1,18 +1,3 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 
 package main
 
@@ -20,9 +5,38 @@ import (
 	"errors"
 	"encoding/json"
 	"fmt"
-	"./entities"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
+
+
+
+
+
+// DATA MODEL
+
+type CarOwner struct {
+	OwnerID   	string 	`json:"userID"`
+	Name 			string 	`json:"name"`
+	Address 	string 	`json:"address"`
+	LicenseID string 	`json:"licenseID"`
+	Telephone string 	`json:"telephone"`
+	Verified  bool    `json:"telephone"`
+}
+
+type Car struct {
+	CarID      	string 	`json:"carID"`
+}
+
+type TestData struct {
+	CarOwners	[]CarOwner 	 `json:"carOwners"`
+	Cars 		[]Car  				`json:"cars"`
+}
+
+
+
+
+
+
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
@@ -51,7 +65,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 }
 
 func (t *SimpleChaincode) addTestdata(stub shim.ChaincodeStubInterface, testDataAsJson string) error {
-	var testData entities.TestData
+	var testData TestData
 	err := json.Unmarshal([]byte(testDataAsJson), &testData)
 	if err != nil {
 		return errors.New("Error while unmarshalling testdata")
