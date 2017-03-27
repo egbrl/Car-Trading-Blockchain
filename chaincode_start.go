33466e14293617a2eef3bdc7a049d6959034ede3
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-03-27 15:57:31 +0200

    print test dataush

--- chaincode_start.go
+++ chaincode_start.go
@@ -44,7 +44,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	errors.New(args)
+	errors.New(args[0])
 
 	// Create test cars
 	t.addTestdata(stub, args[0])
