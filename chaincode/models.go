b7e94beae59d075e2da63c0db763761936a22bf1
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-06-02 21:16:05 +0200

    adds Certificate and CarAudit models

--- chaincode/models.go
+++ chaincode/models.go
@@ -1,6 +1,32 @@
 package main
 
+/*
+ * Fahrzeugausweis
+ */
+type Certificate struct {
+    User            string `json:"user"`            // the name of a user (garage or private person)
+    Insurer         string `json:"insurer"`         // the name of an insurance company
+    Number_Plate    string `json:"number_plate"`    // number plate like 'AG 104 739'
+    Serial_Number   string `json:"serial_number"`   // serial number like 'WVW ZZZ 6RZ HY26 0780'
+    Color           string `json:"color"`
+    Type            string `json:"type"`            // type, like 'passenger car' or 'truck'
+    Brand           string `json:"brand"`
+}
+
+/*
+ * Pruefungsbericht
+ * (Form. 13.20 A)
+ */
+type CarAudit struct {
+    Car                     Car `json:"car"`
+    Number_of_Doors         string `json:"number_of_doors"`      // '4+1' for a standard passenger car
+    Number_of_Cylinders     int `json:"number_of_cylinders"`     // 3, 4, 6, 8 ?
+    Number_of_Axis          int `json:"number_of_axis"`          // typically 2
+    Max_Speed               int `json:"max_speed"`               // maximum speed as tested
+}
+
 type Car struct {
+    Certificate     Certificate `json:"certificate"`
     Name      string `json:"name"`
     Color     string `json:"color"`
     Size      int    `json:"size"`
