8fd2d2fe918d3c1eb9c4f030aee20c62e16b9205
Chr1sGit <chrisherzog@gmx.ch>
2017-06-13 12:45:45 +0200

    confirmation of car added

--- chaincode/car.go
+++ chaincode/car.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "encoding/json"
-    "time"
-    "errors"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
 
-    "github.com/hyperledger/fabric/core/chaincode/shim"
-    pb "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 /*
  * Returns the car index
  */
 func (t *CarChaincode) getCarIndex(stub shim.ChaincodeStubInterface) (map[string]string, error) {
-    response := t.read(stub, carIndexStr)
-    carIndex := make(map[string]string)
-    err := json.Unmarshal(response.Payload, &carIndex)
-    if err != nil {
-        return nil, errors.New("Error parsing car index")
-    }
+	response := t.read(stub, carIndexStr)
+	carIndex := make(map[string]string)
+	err := json.Unmarshal(response.Payload, &carIndex)
+	if err != nil {
+		return nil, errors.New("Error parsing car index")
+	}
 
-    return carIndex, nil
+	return carIndex, nil
 }
 
 /*
@@ -30,11 +30,11 @@ func (t *CarChaincode) getCarIndex(stub shim.ChaincodeStubInterface) (map[string
  * Returns username of car owner with VIN 'vin'.
  */
 func (t *CarChaincode) getOwner(stub shim.ChaincodeStubInterface, vin string) (string, error) {
-    carIndex, err := t.getCarIndex(stub)
-    if err != nil {
-        return "", err
-    }
-    return carIndex[vin], nil
+	carIndex, err := t.getCarIndex(stub)
+	if err != nil {
+		return "", err
+	}
+	return carIndex[vin], nil
 }
 
 /*
@@ -49,76 +49,76 @@ func (t *CarChaincode) getOwner(stub shim.ChaincodeStubInterface, vin string) (s
  * returns the car.
  */
 func (t *CarChaincode) create(stub shim.ChaincodeStubInterface, username string, args []string) pb.Response {
-    if len(args) != 1 {
-        return shim.Error("'create' expects Car with VIN as json")
-    }
+	if len(args) != 1 {
+		return shim.Error("'create' expects Car with VIN as json")
+	}
 
-    // create car from arguments
-    car := Car {}
-    err := json.Unmarshal([]byte(args[0]), &car)
-    if err != nil {
-        return shim.Error("Error parsing car data. Expecting Car with VIN as json.")
-    }
+	// create car from arguments
+	car := Car{}
+	err := json.Unmarshal([]byte(args[0]), &car)
+	if err != nil {
+		return shim.Error("Error parsing car data. Expecting Car with VIN as json.")
+	}
 
-    // add car birth date
-    car.CreatedTs = time.Now().Unix()
+	// add car birth date
+	car.CreatedTs = time.Now().Unix()
 
-    // create user from arguments
-    user := User {}
+	// create user from arguments
+	user := User{}
 
-    // check for existing garage user with that name
-    response := t.read(stub, username)
-    existingUser := User {}
-    err = json.Unmarshal(response.Payload, &existingUser)
-    if err == nil {
-        user = existingUser
-    } else {
-        user.Name = username
-    }
+	// check for existing garage user with that name
+	response := t.read(stub, username)
+	existingUser := User{}
+	err = json.Unmarshal(response.Payload, &existingUser)
+	if err == nil {
+		user = existingUser
+	} else {
+		user.Name = username
+	}
 
-    // check for an existing car with that vin in the car index
-    owner, err := t.getOwner(stub, car.Vin)
-    if err != nil {
-        return shim.Error(err.Error())
-    } else if owner != "" {
-        return shim.Error(fmt.Sprintf("Car with vin '%s' already exists. Choose another vin.", car.Vin))
-    }
+	// check for an existing car with that vin in the car index
+	owner, err := t.getOwner(stub, car.Vin)
+	if err != nil {
+		return shim.Error(err.Error())
+	} else if owner != "" {
+		return shim.Error(fmt.Sprintf("Car with vin '%s' already exists. Choose another vin.", car.Vin))
+	}
 
-    // save car to ledger, the car vin serves
-    // as the index to find the car again
-    carAsBytes, _ := json.Marshal(car)
-    err = stub.PutState(car.Vin, carAsBytes)
-    if err != nil {
-        return shim.Error("Error writing car")
-    }
+	// save car to ledger, the car vin serves
+	// as the index to find the car again
+	carAsBytes, _ := json.Marshal(car)
+	err = stub.PutState(car.Vin, carAsBytes)
+	if err != nil {
+		return shim.Error("Error writing car")
+	}
 
-    // map the car to the users name
-    carIndex, err := t.getCarIndex(stub)
-    if err != nil {
-        return shim.Error(err.Error())
-    }
-    carIndex[car.Vin] = user.Name
-    fmt.Printf("Added car with VIN '%s' created at '%d' in garage '%s' to car index.\n",
-                car.Vin, car.CreatedTs, user.Name)
+	// map the car to the users name
+	carIndex, err := t.getCarIndex(stub)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	carIndex[car.Vin] = user.Name
+	fmt.Printf("Added car with VIN '%s' created at '%d' in garage '%s' to car index.\n",
+		car.Vin, car.CreatedTs, user.Name)
 
-    // write udpated car index back to ledger
-    indexAsBytes, _ := json.Marshal(carIndex)
-    err = stub.PutState(carIndexStr, indexAsBytes)
-    if err != nil {
-        return shim.Error("Error writing car index")
-    }
+	// write udpated car index back to ledger
+	indexAsBytes, _ := json.Marshal(carIndex)
+	err = stub.PutState(carIndexStr, indexAsBytes)
+	if err != nil {
+		return shim.Error("Error writing car index")
+	}
 
-    // hand over the car and write user to ledger
-    user.Cars = append(user.Cars, car.Vin)
-    userAsBytes, _ := json.Marshal(user)
-    err = stub.PutState(user.Name, userAsBytes)
-    if err != nil {
-        return shim.Error("Error writing user")
-    }
+	// hand over the car and write user to ledger
+	user.Cars = append(user.Cars, car.Vin)
+	userAsBytes, _ := json.Marshal(user)
+	err = stub.PutState(user.Name, userAsBytes)
+	if err != nil {
+		return shim.Error("Error writing user")
+	}
 
-    // car creation successfull,
-    // return the car
-    return shim.Success(carAsBytes)
+	// car creation successfull,
+	// return the car
+	return shim.Success(carAsBytes)
 }
 
 /*
@@ -130,25 +130,78 @@ func (t *CarChaincode) create(stub shim.ChaincodeStubInterface, username string,
  * returns the car.
  */
 func (t *CarChaincode) readCar(stub shim.ChaincodeStubInterface, username string, vin string) pb.Response {
-    if vin == "" {
-        return shim.Error("'readCar' expects a non-empty VIN to do the look up")
-    }
+	if vin == "" {
+		return shim.Error("'readCar' expects a non-empty VIN to do the look up")
+	}
 
-    // fetch the car from the ledger
-    carResponse := t.read(stub, vin)
-    car := Car {}
-    err := json.Unmarshal(carResponse.Payload, &car)
-    if err != nil {
-        return shim.Error("Failed to fetch car with vin '" + vin + "' from ledger")
-    }
+	// fetch the car from the ledger
+	carResponse := t.read(stub, vin)
+	car := Car{}
+	err := json.Unmarshal(carResponse.Payload, &car)
+	if err != nil {
+		return shim.Error("Failed to fetch car with vin '" + vin + "' from ledger")
+	}
 
-    // fetch the car index to check if the user owns the car
-    owner, err := t.getOwner(stub, vin)
-    if err != nil {
-        return shim.Error(err.Error())
-    } else if owner != username {
-        return shim.Error("Forbidden: this is not your car")
-    }
+	// fetch the car index to check if the user owns the car
+	owner, err := t.getOwner(stub, vin)
+	if err != nil {
+		return shim.Error(err.Error())
+	} else if owner != username {
+		return shim.Error("Forbidden: this is not your car")
+	}
 
-    return shim.Success(carResponse.Payload)
+	return shim.Success(carResponse.Payload)
+}
+
+/*
+ * Confirms a car.
+ *
+ * Only the owner of a car can request confirmation of a car.
+ * Car needs to be insured as a requirement for confirmation.
+ *
+ *
+ */
+func (t *CarChaincode) confirm(stub shim.ChaincodeStubInterface, username string, args []string) pb.Response {
+	vin := args[0]
+	numberplate := args[1]
+
+	if vin == "" {
+		return shim.Error("'readCar' expects a non-empty VIN to do the look up")
+	}
+
+	// fetch the car from the ledger
+	carResponse := t.read(stub, vin)
+	car := Car{}
+	err := json.Unmarshal(carResponse.Payload, &car)
+	if err != nil {
+		return shim.Error("Failed to fetch car with vin '" + vin + "' from ledger")
+	}
+
+	// check if username is owner of the car
+	if car.Certificate.Username != username {
+		return shim.Error("The person: '" + username + "' is not the owner of the car")
+	}
+
+	// check if car is insured
+	if !(IsInsured(&car)) {
+		return shim.Error("Car is not insured. Please insure car first before trying to confirm it")
+	}
+
+	// check numberplate argument
+	if numberplate == "" {
+		return shim.Error("Car numberplate is empty. Please hand over a numberplate to confirm a car")
+	}
+
+	// check if numberplate is already in use
+	// carIndex, err := t.getCarIndex(stub)
+	// for k, v := range carIndex {
+	// 	if v.Numberplate == numberplate {
+	// 		return shim.Error("Car numberplate already in use. Please use another one!")
+	//
+	// }
+
+	// assign the numberplate to the car
+	car.Certificate.Numberplate = numberplate
+
+	return shim.Success(carResponse.Payload)
 }
--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -1,59 +1,59 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
-    "reflect"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
 
-    "github.com/hyperledger/fabric/core/chaincode/shim"
-    pb "github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 type CarChaincode struct {
 }
 
-const carIndexStr string     = "_cars"
+const carIndexStr string = "_cars"
 const insurerIndexStr string = "_insurers"
 
 func (t *CarChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-    fmt.Println("Car demo Init")
-    
-    var aval int
-    var err error
+	fmt.Println("Car demo Init")
 
-    _, args := stub.GetFunctionAndParameters()
-    if len(args) != 1 {
-        return shim.Error("Incorrect number of arguments. Expecting 1 integer to test chain.")
-    }
+	var aval int
+	var err error
 
-    // initialize the chaincode
-    aval, err = strconv.Atoi(args[0])
-    if err != nil {
-        return shim.Error("Expecting integer value for asset holding")
-    }
+	_, args := stub.GetFunctionAndParameters()
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1 integer to test chain.")
+	}
 
-    // write the state to the ledger
-    // make a test var "abc" in order to able to query it and see if it worked
-    err = stub.PutState("abc", []byte(strconv.Itoa(aval)))
-    if err != nil {
-        return shim.Error(err.Error())
-    }
- 
-    // clear the car index
-    err = clearCarIndex(carIndexStr, stub)
-    if err != nil {
-        return shim.Error(err.Error())
-    }
+	// initialize the chaincode
+	aval, err = strconv.Atoi(args[0])
+	if err != nil {
+		return shim.Error("Expecting integer value for asset holding")
+	}
 
-    // clear the insurer index
-    err = clearInsurerIndex(insurerIndexStr, stub)
-    if err != nil {
-        return shim.Error(err.Error())
-    }
+	// write the state to the ledger
+	// make a test var "abc" in order to able to query it and see if it worked
+	err = stub.PutState("abc", []byte(strconv.Itoa(aval)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 
-    fmt.Println("Init terminated")
-    return shim.Success(nil)
+	// clear the car index
+	err = clearCarIndex(carIndexStr, stub)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// clear the insurer index
+	err = clearInsurerIndex(insurerIndexStr, stub)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("Init terminated")
+	return shim.Success(nil)
 }
 
 /*
@@ -63,78 +63,88 @@ func (t *CarChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
  * Unrestricted queries can only be done from test files.
  */
 func (t *CarChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-    function, args := stub.GetFunctionAndParameters()
+	function, args := stub.GetFunctionAndParameters()
 
-    if len(args) < 2 {
-        return shim.Error("Invoke expects 'username' and 'role' as first two args.")
-    }
+	if len(args) < 2 {
+		return shim.Error("Invoke expects 'username' and 'role' as first two args.")
+	}
 
-    username := args[0]
-    role     := args[1]
-    args = args[2:]
+	username := args[0]
+	role := args[1]
+	args = args[2:]
 
-    fmt.Printf("Invoke is running as user '%s' with role '%s'\n", username, role)
-    fmt.Printf("Invoke is running function '%s' with args: %s\n", function, strings.Join(args, ", "))
+	fmt.Printf("Invoke is running as user '%s' with role '%s'\n", username, role)
+	fmt.Printf("Invoke is running function '%s' with args: %s\n", function, strings.Join(args, ", "))
 
-    if function == "create" {
-        if role != "garage" {
-            return shim.Error("'create' expects you to be a garage user")
-        }
-        return t.create(stub, username, args)
-    } else if function == "read" {
-        if len(args) != 1 {
-            return shim.Error("'read' expects a key to do the look up")
-        } else if (reflect.TypeOf(stub).String() != "*shim.MockStub") {
-            // only allow unrestricted queries from the test files
-            return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to do unrestricted queries on the ledger.", role))
-        } else {
-            return t.read(stub, args[0])
-        }
-    } else if function == "readCar" {
-        if len(args) != 1 {
-            return shim.Error("'readCar' expects a car vin to do the look up")
-        } else {
-            return t.readCar(stub, username, args[0])
-        }
-    }  else if function == "register" {
-        if len(args) != 1 {
-            return shim.Error("'register' expects a car vin to register")
-        } else if role != "dot"{
-            // only the DOT is allowed to register new cars
-            return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to register cars.", role))
-        } else {
-            return t.register(stub, username, args[0])
-        }
-    } else if function == "insureProposal" {
-        if len(args) != 2 {
-            return shim.Error("'insureProposal' expects a car vin and an insurance company")
-        } else if role != "user"{
-            // only normal users are allowed to do insurance proposals
-            return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to create an insurance proposal.", role))
-        } else {
-            return t.insureProposal(stub, username, args[0], args[1])
-        }
-    } else if function == "insuranceAccept" {
-        if len(args) != 2 {
-            return shim.Error("'insuranceAccept' expects a car vin and an insurance company")
-        } else if role != "insurer"{
-            // only normal users are allowed to do insurance proposals
-            return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to create an insurance proposal.", role))
-        } else {
-            return t.insuranceAccept(stub, username, args[0], args[1])
-        }
-    } else if function == "getInsurer" {
-        if len(args) != 1 {
-            return shim.Error("'getInsurer' expects an insurance company name")
-        } else if role != "insurer"{
-            // only normal users are allowed to do insurance proposals
-            return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to create an insurance proposal.", role))
-        } else {
-            return t.getInsurer(stub, args[0])
-        }
-    }
+	if function == "create" {
+		if role != "garage" {
+			return shim.Error("'create' expects you to be a garage user")
+		}
+		return t.create(stub, username, args)
+	} else if function == "read" {
+		if len(args) != 1 {
+			return shim.Error("'read' expects a key to do the look up")
+		} else if reflect.TypeOf(stub).String() != "*shim.MockStub" {
+			// only allow unrestricted queries from the test files
+			return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to do unrestricted queries on the ledger.", role))
+		} else {
+			return t.read(stub, args[0])
+		}
+	} else if function == "readCar" {
+		if len(args) != 1 {
+			return shim.Error("'readCar' expects a car vin to do the look up")
+		} else {
+			return t.readCar(stub, username, args[0])
+		}
+	} else if function == "register" {
+		if len(args) != 1 {
+			return shim.Error("'register' expects a car vin to register")
+		} else if role != "dot" {
+			// only the DOT is allowed to register new cars
+			return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to register cars.", role))
+		} else {
+			return t.register(stub, username, args[0])
+		}
+	} else if function == "confirm" {
+		if len(args) != 2 {
+			return shim.Error("'confirm' expects a car vin to confirm a car")
+		} else if role != "dot" {
+			// only the DOT is allowed to confirm cars
+			return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to confirm cars.", role))
+		} else {
+			return t.confirm(stub, username, args)
+		}
 
-    return shim.Error("Invoke did not find function: " + function)
+	} else if function == "insureProposal" {
+		if len(args) != 2 {
+			return shim.Error("'insureProposal' expects a car vin and an insurance company")
+		} else if role != "user" {
+			// only normal users are allowed to do insurance proposals
+			return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to create an insurance proposal.", role))
+		} else {
+			return t.insureProposal(stub, username, args[0], args[1])
+		}
+	} else if function == "insuranceAccept" {
+		if len(args) != 2 {
+			return shim.Error("'insuranceAccept' expects a car vin and an insurance company")
+		} else if role != "insurer" {
+			// only normal users are allowed to do insurance proposals
+			return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to create an insurance proposal.", role))
+		} else {
+			return t.insuranceAccept(stub, username, args[0], args[1])
+		}
+	} else if function == "getInsurer" {
+		if len(args) != 1 {
+			return shim.Error("'getInsurer' expects an insurance company name")
+		} else if role != "insurer" {
+			// only normal users are allowed to do insurance proposals
+			return shim.Error(fmt.Sprintf("Sorry, role '%s' is not allowed to create an insurance proposal.", role))
+		} else {
+			return t.getInsurer(stub, args[0])
+		}
+	}
+
+	return shim.Error("Invoke did not find function: " + function)
 }
 
 /*
@@ -149,21 +159,21 @@ func (t *CarChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
  * returns ledger state in bytes at position 'key'.
  */
 func (t *CarChaincode) read(stub shim.ChaincodeStubInterface, key string) pb.Response {
-    if key == "" {
-        return shim.Error("'read' expects a non-empty key to do the look up")
-    }
+	if key == "" {
+		return shim.Error("'read' expects a non-empty key to do the look up")
+	}
 
-    valAsBytes, err := stub.GetState(key)
-    if err != nil {
-        return shim.Error("Failed to fetch value at key '" + key + "' from ledger")
-    }
+	valAsBytes, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error("Failed to fetch value at key '" + key + "' from ledger")
+	}
 
-    return shim.Success(valAsBytes)
+	return shim.Success(valAsBytes)
 }
 
 func main() {
-    err := shim.Start(new(CarChaincode))
-    if err != nil {
-        fmt.Printf("Error starting Simple chaincode: %s", err)
-    }
+	err := shim.Start(new(CarChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
 }
