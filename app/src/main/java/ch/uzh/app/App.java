db061d560b0439e7f9feae79024acdb1e7eb6281
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-07-01 15:49:24 +0200

    creates hfc and hfca clients

--- app/src/main/java/ch/uzh/app/App.java
+++ app/src/main/java/ch/uzh/app/App.java
@@ -3,13 +3,12 @@ package ch.uzh.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.hyperledger.fabric.sdk.HFClient;
+import org.hyperledger.fabric_ca.sdk.HFCAClient;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
 import org.hyperledger.fabric.sdk.exception.CryptoException;
 import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
 
-public class App 
-{
-
+public class App {
 	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     public static void main( String[] args ) {
@@ -22,5 +21,16 @@ public class App
 	    } catch (InvalidArgumentException ex) {
 	      LOGGER.error("InvalidArgumentException setting crypto suite", ex);
 	    }
+
+	    LOGGER.info(client.getCryptoSuite().toString());
+
+	    HFCAClient hfcaClient = null;
+	    try {
+	    	hfcaClient = HFCAClient.createNewInstance("http://localhost:7054", null);
+	    } catch (Exception e) {
+	    	LOGGER.error(e.getMessage());
+	    }
+
+	    LOGGER.info(hfcaClient.toString());
     }
 }
