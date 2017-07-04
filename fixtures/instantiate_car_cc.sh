70300f6463f7b4334514e7817e647f78831633cb
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-07-04 12:46:28 +0200

    uses localhost

--- fixtures/instantiate_car_cc.sh
+++ fixtures/instantiate_car_cc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CAR_API_CONTAINER_NAME=car_cc_api
-DOCKER_API_IP=`docker inspect -f '{{ range .NetworkSettings.Networks }}{{ .IPAddress }}{{end}}' $CAR_API_CONTAINER_NAME`
+DOCKER_API_IP=127.0.0.1
 
 curl http://$DOCKER_API_IP:8080/rest/setupclient;
 curl http://$DOCKER_API_IP:8080/rest/getconfig;
