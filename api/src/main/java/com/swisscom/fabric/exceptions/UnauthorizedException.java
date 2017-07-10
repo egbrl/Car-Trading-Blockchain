793cd4bf29225e3202985c8c9bb18709cb138cd8
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-07-10 13:33:41 +0200

    removes com.swisscom

--- api/src/main/java/com/swisscom/fabric/exceptions/UnauthorizedException.java
+++
@@ -1,6 +0,0 @@
-package com.swisscom.fabric.exceptions;
-
-@SuppressWarnings({"squid:UndocumentedApi", "squid:S2166"})
-public class UnauthorizedException extends ServiceException {
-	private static final long serialVersionUID = 1L;
-}
