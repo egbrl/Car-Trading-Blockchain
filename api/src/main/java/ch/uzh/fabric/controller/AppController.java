ddc4aa4a17832a6eb5daba90f7ff412d6af57e6b
Chris <chris.herzog@gmx.ch>
2017-07-11 23:31:54 +0200

    garage + import-car page added

--- api/src/main/java/ch/uzh/fabric/controller/AppController.java
+++ api/src/main/java/ch/uzh/fabric/controller/AppController.java
@@ -154,6 +154,15 @@ public class AppController {
 		return "basic-template";
 	}
 
+	@RequestMapping("/garage")
+	public String garage(){
+		return "garage";
+	}
+
+	@RequestMapping("/garage/import-car")
+	public String importCar(){
+		return "garage/import-car";
+	}
 
 
 
