0f1487be2451428094dbe34ae0c5a2049024f29f
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-07-14 10:48:18 +0200

    removes unused exceptions

--- app/src/main/java/ch/uzh/fabric/exceptions/UnauthorizedException.java
+++
@@ -1,6 +0,0 @@
-package ch.uzh.fabric.exceptions;
-
-@SuppressWarnings({"squid:UndocumentedApi", "squid:S2166"})
-public class UnauthorizedException extends ServiceException {
-	private static final long serialVersionUID = 1L;
-}
