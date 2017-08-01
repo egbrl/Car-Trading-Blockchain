45521b2eee000c7a7056350f08caaee8059ff670
Andreas Gruhler <andreas.gruhler@uzh.ch>
2017-08-01 10:11:37 +0200

    save remaining balance and test

--- chaincode/src/github.com/car_cc/user_test.go
+++ chaincode/src/github.com/car_cc/user_test.go
@@ -9,7 +9,7 @@ import (
     "github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-func TestCreateUserAndUpdateBalance(t *testing.T) {
+func TestUser(t *testing.T) {
     var root string = "test"
     var user string = "test2"
 
@@ -19,7 +19,8 @@ func TestCreateUserAndUpdateBalance(t *testing.T) {
 
     ccSetup(t, stub)
 
-    // create user 'test2'
+    // create user 'test' and 'test2'
+    stub.MockInvoke(uuid, util.ToChaincodeArgs("createUser", root, "user", root))
     response := stub.MockInvoke(uuid, util.ToChaincodeArgs("createUser", root, "user", user))
 
     userObject := User {}
@@ -59,5 +60,34 @@ func TestCreateUserAndUpdateBalance(t *testing.T) {
 
     if updatedBalance != -5 {
         t.Error("Wrong balance")
-    }   
+    }
+
+    // delete user 'test2'
+    response = stub.MockInvoke(uuid, util.ToChaincodeArgs("deleteUser", user, "user", user, root))
+    if response.Payload != nil {
+        t.Error("Error deleting user")
+        return
+    }
+
+    // check that user was deleted
+    response = stub.MockInvoke(uuid, util.ToChaincodeArgs("readUser", user, "user"))
+    if response.Status != 500 {
+        t.Error("User not deleted")
+        return
+    }
+
+    // read the root user
+    response = stub.MockInvoke(uuid, util.ToChaincodeArgs("readUser", root, "user"))
+    userObject = User {}
+    err = json.Unmarshal(response.Payload, &userObject)
+    if err != nil {
+        fmt.Println(response.Payload)
+        t.Error("Error reading root user")
+        return
+    }
+
+    if userObject.Balance != -5 {
+        fmt.Println(userObject.Balance)
+        t.Error("User balance not transferred successfully after deleting user")
+    }
 }
